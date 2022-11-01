4aa0cd5e4dd071bb1a0920ef2d5964b158efbae1
kevin cheung <kevin.cheung.kai@gmail.com>
2022-10-31 22:53:39 -0400

    added function to check latitude and longitude +/- 5 of my longitude/latitude and also added function to check if the current time is night

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
+MY_LAT = 39.176941
+MY_LONG = -77.270157
+MY_EMAIL = "from@example.com"
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-time_now = datetime.now()
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get(
+        "https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset and time_now <= sunrise:
+        return True
+
+
+if is_iss_overhead() and is_night():
+    with smtplib.SMTP(host="smtp.mailtrap.io", port=2525) as connection:
+        connection.starttls()
+        connection.login(user='2a34c59a769644', password='18e850b3615af5')
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs='Kevin.Cheung.Kai@gmail.com',
+                            msg='Subject: Hello\n\nLook up!'
+                            )
+
 # BONUS: run the code every 60 seconds.
-
-
-
